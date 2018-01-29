42a12039fc9337f7a2df0c25748c6746ff0434f4
Ntms Pavan <pavan.nalam3693@gmail.com>
2018-01-29 17:59:02 -0600

    Create Subinside.java

---
+++ Subinside.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+class Subinside
+{
+Public static void main (String [] args)
+  {
+  String s1 = "abcde";
+  String s2 = "cd";
+  int index = -1;
+  int count = 0;
+  for ( int i=0; i<s1.length;i++)
+    {
+      if (s1.charAt(i)==s2.charAt(0) && count == 0)
+        {
+          index = i;
+          count++;      
+        }
+      else if ( s1.charAt(i) == s2.charAt(0))
+        {
+          count++;
+        }
+       else 
+        {
+          index = -1;
+          count = 0;
+         }
+        if (count == s2.length())
+          {
+           break;
+          }
+  
+    }
+  
+  if ( index > 1)
+    System.out.println (" Matched at index" +index);
+  else
+  System.out.println (" S2 string is not a sub string of S1");
+ 
+  }
+
+}
